fd837eea3df628bf76397ad0d808a3927cc38dfd
Kerimov Bulat <Bulatker@gmail.com>
2019-02-17 02:44:49 +0100

    Added kinematics

--- src/robot.py
+++ src/robot.py
@@ -1,65 +1,141 @@
 from src.sensor import Sensor
 from src.wall import Wall
 from src.helper import *
+import math
+import numpy as np
+
 
 class Robot():
-	def __init__(self, WIDTH, HEIGHT, walls):
-		self.x = 100
-		self.y = 200
-		self.theta = 10
-		self.radius = 30
-		self.speed = [5, -5]
+    def __init__(self, WIDTH, HEIGHT, walls):
+        self.x = 100
+        self.y = 200
+        self.theta = 10
+        self.radius = 30
+        self.speed = [2, 2]
+        self.is_rotating = self.check_if_rotates()
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.walls = walls
 
-		self.width = WIDTH
-		self.height = HEIGHT
-		self.walls = walls
+        self.sensors = []
+        num_sensors = 12
+        for i in range(num_sensors):
+            self.sensors.append(Sensor(i * (360 / num_sensors)))
+        self.check_sensors()
 
-		self.sensors = []
-		num_sensors = 12
-		for i in range(num_sensors):
-			self.sensors.append(Sensor(i*(360/num_sensors)))
-		self.check_sensors()
+    def get_pos(self):
+        return int(self.x), int(self.y)
 
-	def get_pos(self):
-		return int(self.x), int(self.y)
+    def check_sensors(self):
+        for sensor in self.sensors:
+            sensor_bearing = (sensor.angle + self.theta) % 360  # Sensor angle is relative to the robot
+            sensor.value = sensor.MAX_SENSOR_VALUE
+            sensor.p2 = point_from_angle(self.x, self.y, sensor_bearing, sensor.value)
+            for wall in self.walls:
+                crossover = wall.intersectsLine((self.x, self.y), sensor.p2)
+                if crossover != False:
+                    dist = distance((self.x, self.y), crossover)
+                    sensor.value = dist
+                    sensor.p2 = crossover
 
-	def check_sensors(self):
-		for sensor in self.sensors:
-			sensor_bearing = (sensor.angle + self.theta) % 360  # Sensor angle is relative to the robot
-			sensor.value = sensor.MAX_SENSOR_VALUE
-			sensor.p2 = point_from_angle(self.x, self.y, sensor_bearing, sensor.value)
-			for wall in self.walls:
-				crossover = wall.intersectsLine((self.x, self.y), sensor.p2)
-				if crossover != False:
-					dist = distance((self.x, self.y), crossover)
-					sensor.value = dist
-					sensor.p2 = crossover
+            # Check for wall collisions
+            if self.radius > sensor.value + 0.0005:
+                moveback = point_from_angle(self.x, self.y, sensor_bearing, -(self.radius - sensor.value))
+                self.x = moveback[0]
+                self.y = moveback[1]
 
-			# Check for wall collisions
-			if self.radius > sensor.value + 0.0005:
-				moveback = point_from_angle(self.x, self.y, sensor_bearing, -(self.radius - sensor.value))
-				self.x = moveback[0]
-				self.y = moveback[1]
+                self.check_sensors()
 
-				self.check_sensors()
+    def update_position(self, time):
+        """ Apply the wheel velocity to the robot position using the elapsed time """
+        self.x += self.speed[0] * time
+        self.y += self.speed[1] * time
 
-	def update_position(self, time):
-		""" Apply the wheel velocity to the robot position using the elapsed time """
-		self.x += self.speed[0] * time
-		self.y += self.speed[1] * time
+        self.theta += time * 5
+        if self.theta >= 360:
+            self.theta = 0
 
-		self.theta += time*5
-		if self.theta > 360:
-			self.theta = 0
-		# TODO: For bulat, play with you physics here
-		# if self.rect.left <= 0 or self.rect.right >= self.width:
-		# 	# When colliding with wall on the left, rebound
-		# 	self.speed[0] = -self.speed[0]
-		# 	self.rect.centerx += self.speed[0] * time
-		# if self.rect.top <= 0 or self.rect.bottom >= self.height:
-		# 	# When colliding with wall on the right, rebound
-		# 	self.speed[1] = -self.speed[1]
-		# 	self.rect.centery += self.speed[1] * time
+        # TODO: For mr. Bulat, play with your physics here
+        # if self.rect.left <= 0 or self.rect.right >= self.width:
+        # 	# When colliding with wall on the left, rebound
+        # 	self.speed[0] = -self.speed[0]
+        # 	self.rect.centerx += self.speed[0] * time
+        # if self.rect.top <= 0 or self.rect.bottom >= self.height:
+        # 	# When colliding with wall on the right, rebound
+        # 	self.speed[1] = -self.speed[1]
+        # 	self.rect.centery += self.speed[1] * time
 
-		# update sensors for new pos
-		self.check_sensors()
+        # update sensors for new pos
+        self.check_sensors()
+
+    def calculate_speed(self):
+        """
+        Calculate the speed of the robot basing on the wheel speed
+        :return: Velocity of the robot
+        """
+        return (self.speed[0] + self.speed[1]) / 2
+
+    def calculate_R(self):
+        """
+        Calculate distance from midpoint to ICC basing on the wheel speed and width
+        :return:
+        """
+        return self.width / 2 * (self.speed[0] + self.speed[1]) / (self.speed[1] - self.speed[0])
+
+    def calculate_rate_of_rotation(self):
+        """
+        Calculate rate of rotation basing on speed of the wheels
+        :return:
+        """
+        return (self.speed[1] - self.speed[0]) / self.width
+
+    def get_ICC_coordinates(self, R):
+        """
+        Calculate the coordinates of ICC
+        :param R: Distance from midpoint to ICC
+        :param theta: Angle of the robot
+        :param x: X coordinate of the middle point
+        :param y: Y coordinate of the middle point
+        :return: Coordinates of the ICC
+        """
+        R = self.calculate_R()
+        return (self.x - R * math.sin(self.theta)), (self.y + self.theta * math.cos(self.theta))
+
+    def check_if_rotates(self):
+        """
+        Checks if the velocities of the wheels are equal and returns corresponding True/False value
+        :return:
+        """
+        if self.speed[0] == self.speed[1]:
+            self.is_rotating = False
+        else:
+            self.is_rotating = True
+
+    def update_position_test(self):
+        """
+        Calculate new position via forward kinematics
+        :return:
+        """
+        self.check_if_rotates()
+        if self.is_rotating:
+            omega = self.calculate_rate_of_rotation()
+            R = self.calculate_R()
+            ICC_x, ICC_y = self.get_ICC_coordinates(R)
+            rotation_matrix = np.array([[math.cos(omega), -math.sin(omega), 0],
+                                        [math.sin(omega), math.cos(omega), 0],
+                                        [0, 0, 1]])
+            coordinate_vector = np.array([self.x - ICC_x], [self.y - ICC_y], 0)
+            rotation_origin_vector = np.array([ICC_x, ICC_y, omega])
+
+            result = np.dot(rotation_matrix, coordinate_vector) + rotation_origin_vector
+            self.x = result[0]
+            self.y = result[1]
+            self.theta = result[2]
+        else:
+            self.x += self.speed[0]
+            self.y += self.speed[0]
+
+        self.check_sensors()
+
+
+    # def update_position_TEST(self, time):
