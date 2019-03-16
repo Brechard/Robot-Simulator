bc1ef1ba6065d5fae07730d16520a6f8df7a1a7f
Kerimov Bulat <Bulatker@gmail.com>
2019-03-16 15:21:31 +0100

    Added drawer.py

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -24,155 +24,154 @@ wall_list = rooms.room_1
 
 
 def run_robot_simulation(params):
-	"""
-	Start the simulation of movement of a robot and calculate the fitness using multiple initial positions
-	:params 0 robot: Robot object
-	:params 1 times: how many times calculate update the fitness
-	:params 2 draw: T/F enable GFX
-	:return: fitness of the robot
-	"""
-	robot = params[0]
-	times = params[1]
-	draw = params[2]
-	wall_list = params[3]
-	initial_positions = [(130, 150, 0), (300, 450, 30), (100, HEIGHT - 150, 145)]
-	# initial_positions = [(130, 150, 0)]
-	# initial_positions = [(600, 300, 0)]
-	fitness = []
-	for j in range(len(initial_positions)):
-		# Start simulation of movement
-		bot = Robot(WIDTH, HEIGHT, wall_list, weights=robot.get_NN_weights_flatten())
-		bot.set_pos(initial_positions[j][0], initial_positions[j][1], initial_positions[j][2])
-		if not draw:
-			for i in range(times):
-				bot.update_position()
+    """
+    Start the simulation of movement of a robot and calculate the fitness using multiple initial positions
+    :params 0 robot: Robot object
+    :params 1 times: how many times calculate update the fitness
+    :params 2 draw: T/F enable GFX
+    :return: fitness of the robot
+    """
+    robot = params[0]
+    times = params[1]
+    draw = params[2]
+    wall_list = params[3]
+    initial_positions = [(130, 150, 0), (300, 450, 30), (100, HEIGHT - 150, 145)]
+    # initial_positions = [(130, 150, 0)]
+    # initial_positions = [(600, 300, 0)]
+    fitness = []
+    for j in range(len(initial_positions)):
+        # Start simulation of movement
+        bot = Robot(WIDTH, HEIGHT, wall_list, weights=robot.get_NN_weights_flatten())
+        bot.set_pos(initial_positions[j][0], initial_positions[j][1], initial_positions[j][2])
+        if not draw:
+            for i in range(times):
+                bot.update_position()
 
-				# If robot is stuck, terminate the simulation
-				if bot.n_not_moved > 50:
-					bot.fitness = -100
-					break
-		else:
-			gui = GFX(wall_list=wall_list)
-			gui.set_robot(bot)
-			gui.main(True, max_time=times, kill_when_stuck=True)
-		fitness.append(bot.fitness)
+                # If robot is stuck, terminate the simulation
+                if bot.n_not_moved > 50:
+                    bot.fitness = -100
+                    break
+        else:
+            gui = GFX(wall_list=wall_list)
+            gui.set_robot(bot)
+            gui.main(True, max_time=times, kill_when_stuck=True)
+        fitness.append(bot.fitness)
 
-	# Return the fitness of the robot in the given room
-	# return np.min(fitness)
-	# return np.sum(fitness)
-	return np.mean(fitness)
+    # Return the fitness of the robot in the given room
+    # return np.min(fitness)
+    # return np.sum(fitness)
+    return np.mean(fitness)
 
 
 def calculate_diversity(population):
-	""" Sum of Euclidean distances """
-	diversity = 0
-	for i in (range(len(population))):
-		for j in range(len(population)):
-			if i != j:
-				a = np.asarray(population[i].get_NN_weights_flatten())
-				b = np.asarray(population[j].get_NN_weights_flatten())
-				diversity += np.linalg.norm(a - b)
-	return diversity
+    """ Sum of Euclidean distances """
+    diversity = 0
+    for i in (range(len(population))):
+        for j in range(len(population)):
+            if i != j:
+                a = np.asarray(population[i].get_NN_weights_flatten())
+                b = np.asarray(population[j].get_NN_weights_flatten())
+                diversity += np.linalg.norm(a - b)
+    return diversity
 
 
 def get_best_individual(path):
-	return Robot(WIDTH, HEIGHT, wall_list, main.load_best_weights(path))
+    return Robot(WIDTH, HEIGHT, wall_list, main.load_best_weights(path))
 
 
 def genetics(n_generation=6, population_size=30, n_selected=5, simulation_steps=500,
-			 mutation_rate=0.05, elitism=0.1, robot_rooms=[rooms.room_1, rooms.room_2, rooms.room_3],
-			 load_population='', draw=False):
-	if load_population != '':
-		population = main.load_population(WIDTH, HEIGHT, wall_list, load_population)
-	else:
-		population = []
-		for r in range(population_size):
-			""" Since we are starting the population, we don't send weights so that they are created randomly"""
-			population.append(Robot(WIDTH, HEIGHT, wall_list))
-		print("Initialized random population. size: ", population_size)
+             mutation_rate=0.05, elitism=0.1, robot_rooms=[rooms.room_1, rooms.room_2, rooms.room_3],
+             load_population='', draw=False):
+    if load_population != '':
+        population = main.load_population(WIDTH, HEIGHT, wall_list, load_population)
+    else:
+        population = []
+        for r in range(population_size):
+            """ Since we are starting the population, we don't send weights so that they are created randomly"""
+            population.append(Robot(WIDTH, HEIGHT, wall_list))
+        print("Initialized random population. size: ", population_size)
+    fitness_history = []
+    stats = []
+    multiprocessing = False
+    if not draw and NUM_THREADS > 1:
+        from multiprocessing import Pool as ThreadPool
+        pool = ThreadPool(NUM_THREADS)
+        multiprocessing = True
+    for generation in range(n_generation):
+        # Simulate fitness for each individual
+        fitness = np.zeros((len(robot_rooms), population_size))
+        for i, room in enumerate(robot_rooms):
+            # Execute every robot in every room
+            if not multiprocessing:
+                fitness[i] = np.array(
+                    [run_robot_simulation([robot, simulation_steps, draw, room]) for robot in population])
+                for pos, robot in enumerate(fitness):
+                    print("Robot", pos, "fitness:", robot)
+            else:
+                # Use the thread pool
+                inputs = []
+                for j, robot in enumerate(population):
+                    inputs.append([robot, simulation_steps, False, room])
+                fitness[i] = pool.map(run_robot_simulation, inputs)
 
-	stats = []
-	multiprocessing = False
-	if not draw and NUM_THREADS > 1:
-		from multiprocessing import Pool as ThreadPool
-		pool = ThreadPool(NUM_THREADS)
-		multiprocessing = True
-	for generation in range(n_generation):
-		# Simulate fitness for each individual
-		fitness = np.zeros((len(robot_rooms), population_size))
-		for i, room in enumerate(robot_rooms):
-			# Execute every robot in every room
-			if not multiprocessing:
-				fitness[i] = np.array(
-					[run_robot_simulation([robot, simulation_steps, draw, room]) for robot in population])
-				for pos, robot in enumerate(fitness):
-					print("Robot", pos, "fitness:", robot)
-			else:
-				# Use the thread pool
-				inputs = []
-				for j, robot in enumerate(population):
-					inputs.append([robot, simulation_steps, False, room])
-				fitness[i] = pool.map(run_robot_simulation, inputs)
-
-        [fitness_history[i].append(fitness[i]) for i in range(1)]
 
         # The final fitness of each robot is the average fitness achieved in the different rooms
         fitness = [np.average(fitness[:, robot]) for robot in range(population_size)]
 
-		# Reproduce
-		best_idx = (-np.array(fitness)).argsort()[:n_selected]
-		best_robots = [population[idx] for idx in best_idx]
-		new_population_weights = []
-		num_offspring = int(population_size * (1 - elitism))
-		for i in range(num_offspring):
-			""" Call a crossover function that is going to return the new weights """
-			# crossover between 2 random parents
-			random.shuffle(best_robots)
-			parent_1, parent_2 = best_robots[:2]
-			crossover = crossover_mutation.two_point_crossover(parent_1.get_NN_weights_flatten(),
-															   parent_2.get_NN_weights_flatten())
+        # Reproduce
+        best_idx = (-np.array(fitness)).argsort()[:n_selected]
+        best_robots = [population[idx] for idx in best_idx]
+        new_population_weights = []
+        num_offspring = int(population_size * (1 - elitism))
+        for i in range(num_offspring):
+            """ Call a crossover function that is going to return the new weights """
+            # crossover between 2 random parents
+            random.shuffle(best_robots)
+            parent_1, parent_2 = best_robots[:2]
+            crossover = crossover_mutation.two_point_crossover(parent_1.get_NN_weights_flatten(),
+                                                               parent_2.get_NN_weights_flatten())
 
-			# Mutation
-			mutation = crossover_mutation.mutation_v1(crossover, mutation_rate, 5)
-			new_population_weights.append(mutation)
+            # Mutation
+            mutation = crossover_mutation.mutation_v1(crossover, mutation_rate, 5)
+            new_population_weights.append(mutation)
 
-		# Elitism: Add the best performing individuals to the new population without crossover but with mutation
-		for i in range(population_size - num_offspring):
-			genome = population[best_idx[i]].get_NN_weights_flatten()
-			new_population_weights.append(crossover_mutation.mutation_v1(genome, mutation_rate, 5))
+        # Elitism: Add the best performing individuals to the new population without crossover but with mutation
+        for i in range(population_size - num_offspring):
+            genome = population[best_idx[i]].get_NN_weights_flatten()
+            new_population_weights.append(crossover_mutation.mutation_v1(genome, mutation_rate, 5))
 
-		# Stats
-		max_fitness = np.max(fitness)
-		avg_fitness = np.mean(fitness)
-		print('\033[94m', "Best fitness in generation", generation, "is:", max_fitness,
-			  ", avg fitness:", avg_fitness, '\033[0m')
-		diversity = calculate_diversity(population)
-		print('\033[94m', "The population diversity", generation, "is:", diversity, '\033[0m')
-		stats.append([generation, np.max(fitness), np.mean(fitness), diversity])
+        # Stats
+        max_fitness = np.max(fitness)
+        avg_fitness = np.mean(fitness)
+        print('\033[94m', "Best fitness in generation", generation, "is:", max_fitness,
+              ", avg fitness:", avg_fitness, '\033[0m')
+        diversity = calculate_diversity(population)
+        print('\033[94m', "The population diversity", generation, "is:", diversity, '\033[0m')
+        stats.append([generation, np.max(fitness), np.mean(fitness), diversity])
 
-		# Save
-		main.save_population(population, generation)
-		main.save_best_robot(population[np.argmax(fitness)], generation)
+        # Save
+        main.save_population(population, generation)
+        main.save_best_robot(population[np.argmax(fitness)], generation)
 
-		# Build new population using the new weights
-		population = []
-		for r in range(population_size):
-			population.append(Robot(WIDTH, HEIGHT, wall_list, weights=new_population_weights[r]))
+        # Build new population using the new weights
+        population = []
+        for r in range(population_size):
+            population.append(Robot(WIDTH, HEIGHT, wall_list, weights=new_population_weights[r]))
 
-		if generation > 0 and generation % 5 == 0 or generation == n_generation - 1:
-			fig, ax1 = plt.subplots()
-			ax1.plot(np.array(stats)[:, 1], '-r', label='max fitness')
-			ax1.plot(np.array(stats)[:, 2], '--r', label='avg fitness')
-			ax1.set_xlabel('Generation')
-			ax1.set_ylabel('Fitness', color='r')
-			ax1.tick_params('y', colors='r')
+        if generation > 0 and generation % 5 == 0 or generation == n_generation - 1:
+            fig, ax1 = plt.subplots()
+            ax1.plot(np.array(stats)[:, 1], '-r', label='max fitness')
+            ax1.plot(np.array(stats)[:, 2], '--r', label='avg fitness')
+            ax1.set_xlabel('Generation')
+            ax1.set_ylabel('Fitness', color='r')
+            ax1.tick_params('y', colors='r')
 
-			ax2 = ax1.twinx()
-			ax2.plot(np.array(stats)[:, 3], '-b', label='diversity')
-			ax2.set_ylabel('Diversity', color='b')
+            ax2 = ax1.twinx()
+            ax2.plot(np.array(stats)[:, 3], '-b', label='diversity')
+            ax2.set_ylabel('Diversity', color='b')
 
-			fig.legend()
-			fig.tight_layout()
-			plt.show()
+            fig.legend()
+            fig.tight_layout()
+            plt.show()
 
-	return population[np.argmax(fitness)]
+    return population[np.argmax(fitness)]
--- src/main.py
+++ src/main.py
@@ -7,10 +7,10 @@ DIR = 'weights/' + datetime.datetime.now().strftime("%m%d_%H%M%S")
 
 
 def init_dir(i):
-	dir = os.path.dirname(DIR + '/gen' + str(i) + '/w')
-	if not os.path.exists(dir):
-		print('Saving weights in directory: ', dir)
-		os.makedirs(DIR + '/gen' + str(i))
+    dir = os.path.dirname(DIR + '/gen' + str(i) + '/w')
+    if not os.path.exists(dir):
+        print('Saving weights in directory: ', dir)
+        os.makedirs(DIR + '/gen' + str(i))
 
 
 def load_population(WIDTH, HEIGHT, wall_list, path):
@@ -18,7 +18,7 @@ def load_population(WIDTH, HEIGHT, wall_list, path):
     Load the weights of the population
     @return population: array of robots
     """
-    weights = np.loadtxt(path + '/weights.txt', dtype = int)
+    weights = np.loadtxt(path + '/weights.txt', dtype=int)
     population = []
     for r_weights in weights:
         population.append(Robot(WIDTH, HEIGHT, wall_list, r_weights))
@@ -27,27 +27,27 @@ def load_population(WIDTH, HEIGHT, wall_list, path):
 
 
 def save_population(population, i):
-	"""
-	Save the weights of all the population
-	:param population: Array that contains  all the robots of the population to save
-	"""
-	weights = []
-	for robot in population:
-		weights.append(robot.get_NN_weights_flatten())
+    """
+    Save the weights of all the population
+    :param population: Array that contains  all the robots of the population to save
+    """
+    weights = []
+    for robot in population:
+        weights.append(robot.get_NN_weights_flatten())
 
-	weights = np.array(weights)
-	init_dir(i)
-	np.savetxt(DIR + "/gen" + str(i) + "/weights.txt", weights)
+    weights = np.array(weights)
+    init_dir(i)
+    np.savetxt(DIR + "/gen" + str(i) + "/weights.txt", weights)
 
 
 def save_best_robot(robot, i):
-	init_dir(i)
-	np.savetxt(DIR + "/gen" + str(i) + "/best_robot.txt", robot.get_NN_weights_flatten())
+    init_dir(i)
+    np.savetxt(DIR + "/gen" + str(i) + "/best_robot.txt", robot.get_NN_weights_flatten())
 
 
 def load_best_weights(dir):
-	""" Return the weights of the best robot of the population """
-	return np.loadtxt(dir + '/best_robot.txt', dtype=float)
+    """ Return the weights of the best robot of the population """
+    return np.loadtxt(dir + '/best_robot.txt', dtype=float)
 
 
 import genetic_algorithm as g
@@ -58,17 +58,16 @@ import rooms
 
 
 def main():
-
-	n_generations = 50
-	population_size = 50
-	n_selected = 20
-	elitism = 0.05
-	simulation_steps = 2000
-	draw = False
-	mutation_rate = 0.02
-	# robot_rooms = [rooms.room_1, rooms.room_2, rooms.room_3]
-	robot_rooms = [rooms.room_2, rooms.room_3]
-	# robot_rooms = [rooms.room_3]
+    n_generations = 50
+    population_size = 50
+    n_selected = 20
+    elitism = 0.05
+    simulation_steps = 2000
+    draw = False
+    mutation_rate = 0.02
+    # robot_rooms = [rooms.room_1, rooms.room_2, rooms.room_3]
+    robot_rooms = [rooms.room_2, rooms.room_3]
+    # robot_rooms = [rooms.room_3]
 
     # best_robot = g.genetics(n_generation = n_generations, population_size = population_size, n_selected = n_selected,
     #                         elitism = elitism, simulation_steps = simulation_steps, robot_rooms = robot_rooms,
@@ -78,14 +77,14 @@ def main():
     best_robot = g.get_best_individual('/Users/erytheis/PycharmProjects/Robot-Simulator/src/weights/0312_002820/gen49')
     # best_robot = Robot(g.WIDTH, g.HEIGHT, rooms.room_3)
     gui = GFX()
-    best_robot.set_walls(rooms.room_3)
+    best_robot.set_walls(rooms.room_5)
     best_robot.set_pos(300, 450, 0)
     # best_robot.set_pos(600, 300, 0)
     # best_robot.set_pos(130, 150, 0)
     # best_robot.set_pos(100, g.HEIGHT - 150, 145)
     gui.set_robot(best_robot)
-    gui.main(draw = True, max_time = 50000, kill_when_stuck = False)
+    gui.main(draw=True, max_time=50000, kill_when_stuck=False)
 
 
 if __name__ == "__main__":
-	main()
+    main()
